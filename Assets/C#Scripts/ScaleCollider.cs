89d94f0e5f62c07f2c72031ee76eb61ca4cb9272
Caps <tmandziak02@gmail.com>
2022-11-15 21:15:52 -0600

    Fixed death camera scaling issue

---
+++ Assets/C#Scripts/ScaleCollider.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleCollider : MonoBehaviour
+{
+    public Camera cam;
+    public BoxCollider2D box;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var aspect = (float)Screen.width / (float)Screen.height;
+        var orthoSize = cam.orthographicSize;
+ 
+        var width = 2.0f * orthoSize * aspect;
+        var height = 2.0f * cam.orthographicSize;
+ 
+        box.size = new Vector2(width, height);     
+    }
+
+}
