0158579bb17fc1cb666e6738da93268d967de86b
Ghanim Jamil <ghj729@example.com>
2022-11-19 20:01:28 -0600

    Main menu added, death handling working, nothing fancy

---
+++ Assets/C#Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // SceneManager is a method of SceneManagement
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() {
+    	// When play is pressed, load the game, or the scene 'CyberSurvivors'
+    	SceneManager.LoadScene("CyberSurvivors");
+    }
+
+    public void QuitGame() {
+    	// When quit is pressed, quit the game
+    	Debug.Log("Termination"); // just to check if its actually working in the editor
+    	Application.Quit();
+    }
+}
--- Assets/C#Scripts/MoveCamera.cs
+++ Assets/C#Scripts/MoveCamera.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MoveCamera : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject refObj;
     void Start()
     {
         
@@ -13,15 +15,20 @@ public class MoveCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.right * Time.deltaTime * 0.50f;
+    	if (refObj != null) {
+			transform.position += Vector3.right * Time.deltaTime * 0.50f;    	}
+    	else {
+    		SceneManager.LoadScene("MainMenu");
+    	}
     }
+    
 
     /**
     *If the player exits the Camera, they die
     */
     void OnTriggerExit2D(Collider2D other){
         if(other.transform.tag == "Player"){
-            other.transform.GetComponent<Player>().TakeDamage(1000000000);
+            Destroy(refObj);
         }
     }
 }
